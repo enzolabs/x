a299bc1b7375eb91cfe4b67d14c21dc2b4c7b528
Vishnu Jayadevan <blueyellowpencil@gmail.com>
2022-06-27 18:50:25 +0530

    making the logging interface narrower

--- logger/logger.go
+++ logger/logger.go
@@ -2,18 +2,18 @@ package logger
 
 import "context"
 
-type Interface interface {
-	Debug(ctx context.Context, args ...interface{})
+type LoggerWithContext interface {
+	// Debug(ctx context.Context, args ...interface{})
 	Info(ctx context.Context, args ...interface{})
 	Warn(ctx context.Context, args ...interface{})
 	Error(ctx context.Context, args ...interface{})
-	Fatal(ctx context.Context, args ...interface{})
-	Panic(ctx context.Context, args ...interface{})
+	// Fatal(ctx context.Context, args ...interface{})
+	// Panic(ctx context.Context, args ...interface{})
 
-	Debugf(ctx context.Context, format string, args ...interface{})
+	// Debugf(ctx context.Context, format string, args ...interface{})
 	Infof(ctx context.Context, format string, args ...interface{})
 	Warnf(ctx context.Context, format string, args ...interface{})
 	Errorf(ctx context.Context, format string, args ...interface{})
-	Fatalf(ctx context.Context, format string, args ...interface{})
-	Panicf(ctx context.Context, format string, args ...interface{})
+	// Fatalf(ctx context.Context, format string, args ...interface{})
+	// Panicf(ctx context.Context, format string, args ...interface{})
 }
